d0479014e367d51ed4adff6a82ddbffa5d0a96e0
justanotherbyte <reachvishm@gmail.com>
2026-08-22 14:48:55 +0100

    feat: add og tags

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Viswa Marepalli';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = "Hey there! I'm Viswa, a 1st year Electronic and Information Engineering student at Imperial College London. My interests lie primarily in Robotics, FPGAs and Quantum Computing.";
