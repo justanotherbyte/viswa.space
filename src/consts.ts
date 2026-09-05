90880d95ea2669b4cd98e8f3c1c3b7849cb574dc
Viswa Marepalli <reachvishm@gmail.com>
2026-09-05 14:52:04 +0100

    chore: update to new email

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Viswa Marepalli';
 export const SITE_DESCRIPTION = "Hey there! I'm Viswa, a 1st year Electronic and Information Engineering student at Imperial College London. My interests lie primarily in Robotics, FPGAs and Quantum Computing.";
+export const MY_EMAIL = "hello@viswa.space";
